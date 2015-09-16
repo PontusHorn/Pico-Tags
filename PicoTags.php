3747e4c36842f0b54decd8b311333d398a6ada98
pontus.horn <pontus.horn@gmail.com>
2015-09-16 23:00:18 +0200

    Initial commit of plugin, readme, and .gitignore

---
+++ PicoTags.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Tags plugin for Pico CMS (@see https://github.com/picocms/Pico)
+ *
+ * Using this plugin, you can use the "Tags" and "Filter" headers in the page meta block
+ * in order to modify the "pages" array for certain pages. This creates the possibility
+ * to feature index pages which show only posts of a certain type.
+ *
+ * The "Tags" header accepts a comma-separated list of tags that apply to the current page.
+ *
+ * The "Filter" header also accepts a comma-separated list of tags, but instead specifies
+ * which pages end up in the "pages" array. A page with no "Filter" header will have an
+ * unfiltered list of pages, whereas a page that specifies the header "Filter: foo, bar"
+ * will receive in its "pages" array only pages having at least one of those two tags.
+ *
+ * @author Pontus Horn
+ * @link https://pontushorn.me
+ * @repository https://
+ * @license http://opensource.org/licenses/MIT
+ */
+
+class PicoTags extends AbstractPicoPlugin
+{
+
+    /**
+     * Register the "Tags" and "Filter" meta header fields.
+     *
+     * @see    Pico::getMetaHeaders()
+     * @param  array<string> &$headers list of known meta header fields
+     * @return void
+     */
+    public function onMetaHeaders(&$headers)
+    {
+        $headers['tags'] = 'Tags';
+        $headers['filter'] = 'Filter';
+    }
+
+    /**
+     * Parse the current page's tags and/or filters into arrays.
+     *
+     * @see    Pico::getFileMeta()
+     * @param  array &$meta parsed meta data
+     * @return void
+     */
+    public function onMetaParsed(&$meta)
+    {
+        $meta['tags'] = PicoTags::parseTags($meta['tags']);
+        $meta['filter'] = PicoTags::parseTags($meta['filter']);
+    }
+
+    /**
+     * If the current page has a filter on tags, filter out the $pages array to
+     * only contain pages having any of those tags.
+     *
+     * @see    Pico::getPages()
+     * @see    Pico::getCurrentPage()
+     * @see    Pico::getPreviousPage()
+     * @see    Pico::getNextPage()
+     * @param  array &$pages        data of all known pages
+     * @param  array &$currentPage  data of the page being served
+     * @param  array &$previousPage data of the previous page
+     * @param  array &$nextPage     data of the next page
+     * @return void
+     */
+    public function onPagesLoaded(&$pages, &$currentPage, &$previousPage, &$nextPage)
+    {
+        if ($currentPage && !empty($currentPage['meta']['filter'])) {
+            $tagsToShow = $currentPage['meta']['filter'];
+
+            $pages = array_filter($pages, function ($page) use ($tagsToShow) {
+                $tags = PicoTags::parseTags($page['meta']['tags']);
+                return count(array_intersect($tagsToShow, $tags)) > 0;
+            });
+        }
+    }
+
+    /**
+     * Get array of tags from metadata string.
+     *
+     * @param $tags
+     * @return array
+     */
+    private static function parseTags($tags)
+    {
+        $tags = empty($tags) ? [] : explode(',', $tags);
+        return array_map('trim', $tags);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Pico-Tags
-A plugin for Pico to add tagging functionality.
+# Pico Tags
+
+A plugin for the flat file CMS [Pico](https://github.com/picocms/Pico) Using this plugin, you can use the `Tags` and
+`Filter` headers in the page meta block in order to modify the `pages` array for pages of your choice. This creates the
+possibility to feature index pages which show only posts of a certain type.
+
+The `Tags` header accepts a comma-separated list of tags that apply to the current page.
+
+The `Filter` header also accepts a comma-separated list of tags, but instead specifies which pages end up in the `pages`
+array. A page with no `Filter` header will have an unfiltered list of pages, whereas a page that specifies the header
+`Filter: foo, bar` will receive in its `pages` array only pages having at least one of those two tags.
+
+## Installation
+
+Copy the file `PicoTags.php` to the `plugins` subdirectory of your Pico installation directory. That's it!
+
+## Usage
+
+To assign tags to a page, specify the `Tags` header inside the meta header block at the top of your file, e.g.:
+
+```
+---
+Title: My first blog post
+Date: 2015-09-16 13:37:00
+Tags: foo, bar, blog
+Template: article
+---
+```
+
+To only show pages with certain tags on another page, use the `Filter` header, e.g.:
+
+```
+---
+Title: Blog
+Filter: blog
+Template: article-list
+---
+```
+
+Actually looping through the filtered list of pages (in the above case, pages tagged `blog`) to display them would be
+done in the template file (`article-list.md`).
