805f11cfd1290803efe487ced2496e1f4c969d39
Thomas Tanghus <thomas@tanghus.net>
2019-12-03 15:53:13 +0100

    Add tags_all twig function

--- PicoTags.php
+++ PicoTags.php
@@ -24,6 +24,11 @@ class PicoTags extends AbstractPicoPlugin
 {
 
     /**
+     * All tags used in all pages.
+     */
+    protected $allTags = [];
+
+    /**
      * Register the "Tags" and "Filter" meta header fields.
      *
      * @see    Pico::getMetaHeaders()
@@ -65,6 +70,14 @@ class PicoTags extends AbstractPicoPlugin
      */
     public function onPagesLoaded(&$pages, &$currentPage, &$previousPage, &$nextPage)
     {
+        foreach ($pages as $page) {
+            $tags = PicoTags::parseTags($page['meta']['tags']);
+            if ($page && !empty($tags)) {
+                $this->allTags = array_merge($this->allTags, $tags);
+            }
+            $this->allTags = array_unique($this->allTags);
+        }
+
         if ($currentPage && !empty($currentPage['meta']['filter'])) {
             $tagsToShow = $currentPage['meta']['filter'];
 
@@ -75,6 +88,21 @@ class PicoTags extends AbstractPicoPlugin
         }
     }
 
+    /**
+     * Register Twig's tags_all.
+     * I don't know if it has to be in this method; just copying from http-params ;)
+     *
+     * @see DummyPlugin::onPageRendering()
+     */
+    public function onPageRendering(Twig_Environment &$twig, array &$twigVariables, &$templateName)
+    {
+        $twig->addFunction(new Twig_SimpleFunction('tags_all', array($this, 'getTags')));
+    }
+
+    public function getTags() {
+        return $this->allTags;
+    }
+    
     /**
      * Get array of tags from metadata string.
      *
